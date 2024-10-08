cf28d3fce8a2087809d066c9bd9b5cedc60cca0d
VCGIB\nikolenkob <borys.nikolenko@betvictor.com>
2024-10-08 10:18:23 +0200

    custom login page

---
+++ src/main/java/tech/nvite/security/LoginController.java
@@ -0,0 +1,14 @@
+package tech.nvite.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class LoginController {
+
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+
+}
--- src/main/java/tech/nvite/security/SecurityConfig.java
+++ src/main/java/tech/nvite/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package tech.nvite.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -14,11 +16,12 @@ public class SecurityConfig {
 				.authorizeHttpRequests(authorizeRequests ->
 						authorizeRequests
 								.requestMatchers("/login").permitAll()
+								.requestMatchers("/icons/**", "/css/**", "/js/**", "/images/**").permitAll()
 								.anyRequest().authenticated()
 				)
-				.oauth2Login(oauth2Login ->
-						oauth2Login
-								.loginPage("/login")
+				.oauth2Login(oauth2 -> oauth2
+						.loginPage("/login")
+						.defaultSuccessUrl("/events", true)
 				);
 
 		return http.build();
