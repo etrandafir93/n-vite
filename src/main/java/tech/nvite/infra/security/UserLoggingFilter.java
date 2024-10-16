2fa59e6b2e9d9fef5d7480145f11fdfd67cc9a7e
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-10-16 20:16:55 +0200

    improve logs

---
+++ src/main/java/tech/nvite/infra/security/UserLoggingFilter.java
@@ -0,0 +1,41 @@
+package tech.nvite.infra.security;
+
+import jakarta.servlet.*;
+import org.slf4j.MDC;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+class UserLoggingFilter implements Filter {
+	private static final String USER_KEY = "username";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication != null
+				&& authentication.getPrincipal() != null
+				&& authentication.getPrincipal() instanceof DefaultOAuth2User user) {
+					MDC.put(USER_KEY, user.getAttribute("email"));
+			} else {
+				MDC.put(USER_KEY, "anonymous");
+			}
+			chain.doFilter(request, response);
+		} finally {
+			MDC.remove(USER_KEY);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
