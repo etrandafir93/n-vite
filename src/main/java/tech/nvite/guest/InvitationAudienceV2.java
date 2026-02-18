7dbd1b3391f5a1b3aa24f4efc73a7220a8922eec
borysnikolenko <borysnikolenko@gmail.com>
2026-02-18 15:21:47 +0000

    initial changes

--- src/main/java/tech/nvite/guest/InvitationAudienceV2.java
+++ src/main/java/tech/nvite/guest/InvitationAudienceV2.java
@@ -1,19 +1,78 @@
 package tech.nvite.guest;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import tech.nvite.domain.model.Event;
+import tech.nvite.domain.model.EventReference;
+import tech.nvite.domain.model.InvitationVisitor;
+import tech.nvite.domain.model.RsvpAnswer;
+import tech.nvite.domain.usecases.RsvpInvitationUseCase;
+import tech.nvite.domain.usecases.VisitInvitationUseCase;
 
 @RestController
 @RequestMapping("v2/invitations/{ref}")
+@RequiredArgsConstructor
 public class InvitationAudienceV2 {
 
+  private final VisitInvitationUseCase visitInvitationUseCase;
+  private final RsvpInvitationUseCase rsvpInvitationUseCase;
+
   @GetMapping
-  public InvitationDetailsDto invitationDetails() {
-    return null; // todo
+  public InvitationDetailsDto invitationDetails(
+      @PathVariable String ref, @RequestParam(required = false) String guest) {
+    InvitationVisitor viewer =
+        Optional.ofNullable(guest)
+            .map(InvitationVisitor::withName)
+            .orElseGet(InvitationVisitor::anonymous);
+    var request = new VisitInvitationUseCase.Request(new EventReference(ref), viewer);
+    var response = visitInvitationUseCase.apply(request);
+    return InvitationDetailsDto.from(response.evt());
   }
 
-  record InvitationDetailsDto() {
-    // todo
+  @PostMapping("/responses")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void rsvp(@PathVariable String ref, @RequestBody RsvpRequestDto rsvpRequest) {
+    var rsvpAnswer =
+        "ACCEPTED".equalsIgnoreCase(rsvpRequest.answer())
+            ? new RsvpAnswer.Accepted()
+            : new RsvpAnswer.Declined();
+
+    var request =
+        new RsvpInvitationUseCase.Request(
+            new EventReference(ref), rsvpRequest.guestName(), rsvpAnswer, rsvpRequest.partnerName());
+
+    rsvpInvitationUseCase.apply(request);
+  }
+
+  public record RsvpRequestDto(String guestName, String answer, String partnerName) {}
+
+  public record InvitationDetailsDto(
+      String brideName,
+      String groomName,
+      LocalDateTime eventDate,
+      String eventLocation,
+      String eventReception,
+      String eventReference,
+      String backgroundImageUrl) {
+    public static InvitationDetailsDto from(Event event) {
+      return new InvitationDetailsDto(
+          event.brideName(),
+          event.groomName(),
+          event.eventDateTime(),
+          event.eventLocation(),
+          event.eventReception(),
+          event.reference().value(),
+          event.backgroundImageOrDefault());
+    }
   }
 }
